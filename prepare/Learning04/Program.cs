e303f25ad1cd96e94ff324110506ef11c289ca35
Camtris <chance.ochsenbein@gmail.com>
2025-10-28 09:59:28 -0600

    Finished Prep4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Math math1 = new("Chance Ochsenbein", "Math 316", "1.3.6", "1-10");
+        Writing write1 = new("Chance Ochsenbein", "English 101", "How To Write a Sentence");
+        Console.WriteLine($"{math1.GetSummary()}\n{math1.GetHomeworkList()}\n");
+        Console.WriteLine($"{write1.GetSummary()}\n{write1.GetWritingInformation()}");
     }
 }
